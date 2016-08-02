79bfafcb8406ffe584778cec3cc4d7e3d64683f2
jose1824 <jose1824@live.com.mx>
2016-08-02 15:17:53 -0500

    Menu, icono, toolbar añadidos

--- app/src/main/java/com/example/pc_/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/pc_/controlesbasicos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pc_.controlesbasicos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Titulo de mi toolbar");
         toolbar.setSubtitle("Subtitulo :3");
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
