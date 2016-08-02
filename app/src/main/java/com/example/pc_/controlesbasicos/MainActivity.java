c7772d7c4bd31b09708d671d4c62f7a4874ae9e3
jose1824 <jose1824@live.com.mx>
2016-08-02 15:02:26 -0500

    Agregado getSupportActionBar

--- app/src/main/java/com/example/pc_/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/example/pc_/controlesbasicos/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.pc_.controlesbasicos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        toolbar.setTitle("Titulo de mi toolbar");
+        toolbar.setSubtitle("Subtitulo :3");
+        setSupportActionBar(toolbar);
     }
 }
